4bb662f0901511fbe0ba158274e6520c78548382
Maic Miller <maicmiller42@gmail.com>
2021-03-27 18:48:03 -0300

    :construction: Criação das rotas HTTP
    - Criação da rota GET
    - Criação da rota POST
    - Criação da rota PUT
    - Criação da rota DELETE

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,40 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.get('/projects', (request, response) => {
     // Sempre retornaremos um Vetor ou um Json
-    return response.json({ message: 'Hello Go Stack!' });
+    return response.json([
+        'Project - 1',
+        'Project - 2',
+        'Project - 3'
+    ]);
+});
+
+app.post('/projects', (request, response) => {
+    return response.json([
+        'Project - 1',
+        'Project - 2',
+        'Project - 3',
+        'Project - 4',
+        'Project - 5',
+        'Project - 3'
+    ]);
+});
+
+
+app.put('/projects/:id', (request, response) => {
+    return response.json([
+        'Project - 1',
+        'Project - 2',
+        'Project - 3',
+    ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json([
+        'Project - 1',
+        'Project - 2'
+    ]);
 });
 
 app.listen(3333, () => {
