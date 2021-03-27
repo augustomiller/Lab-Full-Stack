7ef4cb766b2040d14af5d82bd356f86ac1c5bd87
Maic Miller <maicmiller42@gmail.com>
2021-03-27 18:13:52 -0300

    :heavy_plus_sign: Configuração do Nodemon
    - Instalação da dependência nodemon [
    yarn add nodemon -D ];
    - Criando um script de Alies para facilitar na execução do comando nodemon;
    - Configurando um log de feedback para sabermos que o servidor está no ar.

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,9 @@ const app = express();
 
 app.get('/', (request, response) => {
     // Sempre retornaremos um Vetor ou um Json
-    return response.json({ message: 'Hello World!' });
+    return response.json({ message: 'Hello Go Stack!' });
 });
 
-app.listen(3333);
+app.listen(3333, () => {
+    console.log('Back-end started!');
+});
