f9c3e9da500c14d6874a43fc1a29413040e68829
Maic Miller <maicmiller42@gmail.com>
2021-03-28 18:44:22 -0300

    :pencil: Componentes
    - Três partes principais:
       - Componentes;
       - Propriedades;
       - Estado;
    - Inserindo propriedades em um componente filho.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,5 +2,21 @@ import React from 'react';
 import Header from './components/Header';
 
 export default function App() {
-    return <Header />;
+    return (
+        <>
+            <Header title="Homepage">
+                <ul>
+                    <li>Homepage</li>
+                    <li>Projects</li>
+                </ul>
+            </Header>
+            <Header title="Projects">
+                <ul>
+                    <li>Homepage</li>
+                    <li>Projects</li>
+                    <li>login</li>
+                </ul>
+            </Header >
+        </>
+    );
 }
