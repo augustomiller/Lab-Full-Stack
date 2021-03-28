3f2f93b122c7703cd51b0a4bf31faa8122ce6b84
Maic Miller <maicmiller42@gmail.com>
2021-03-28 17:59:02 -0300

    :pencil: Criando o primeiro componente.
    - Criando o primeiro componente, reutilizando-o e aplicando o conceito de "fragment's".

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <header>
+            <>
+                <h1>ReactJS</h1>
+                <h1>ReactJS</h1>
+            </>
+        </header>
+    );
+}
